96f8b410fa1ede1b61a5487940b437dec2106270
Enverst <123556098+EnverstNgau@users.noreply.github.com>
2024-12-08 16:31:21 +0700

    Create MoreCamera.lua

---
+++ Script/Mics/MoreCamera.lua
@@ -0,0 +1,6 @@
+local Players = game:GetService("Players")
+
+local player = Players.LocalPlayer
+
+player.CameraMaxZoomDistance = 9999999999999999999999999999999999999999
+player.CameraMinZoomDistance = 1
