6fe35438524ccae717baf72f5ed87f01f9eb8ea5
Enverst <123556098+EnverstNgau@users.noreply.github.com>
2024-12-08 16:41:34 +0700

    Update AllInOne.lua

--- Script/RockFruits/AllInOne.lua
+++ Script/RockFruits/AllInOne.lua
@@ -1,8 +1,9 @@
-
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/EnverstNgau/skibiditoilwt/refs/heads/main/Script/RockFruits/DeleteRaidMap.lua"))()
-  wait()
-  Max = math.huge
-  Min = 11
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/EnverstNgau/skibiditoilwt/refs/heads/main/Script/Mics/MoreCamera.lua"))()
-  wait() 
-  loadstring(Game:HttpGet("https://raw.githubusercontent.com/EnverstNgau/NewFreeScript/refs/heads/main/Misc/SuperFpsBoost.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/EnverstNgau/skibiditoilwt/refs/heads/main/Script/RockFruits/DeleteRaidMap.lua"))()
+wait()
+Max = math.huge
+Min = 11
+loadstring(game:HttpGet("https://raw.githubusercontent.com/EnverstNgau/skibiditoilwt/refs/heads/main/Script/Mics/MoreCamera.lua"))()
+wait() 
+loadstring(Game:HttpGet("https://raw.githubusercontent.com/EnverstNgau/NewFreeScript/refs/heads/main/Misc/SuperFpsBoost.lua"))()
+wait() 
+setfpscap(90)
