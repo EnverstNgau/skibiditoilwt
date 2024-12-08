fde500173c5b456636f7f741e56f37529b942537
Enverst <123556098+EnverstNgau@users.noreply.github.com>
2024-12-08 16:35:52 +0700

    Create AllInOne.lua

---
+++ Script/RockFruits/AllInOne.lua
@@ -0,0 +1,9 @@
+pcall(function()
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/EnverstNgau/skibiditoilwt/refs/heads/main/Script/RockFruits/DeleteRaidMap.lua"))()
+  wait()
+  Max = math.huge
+  Min = 11
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/EnverstNgau/skibiditoilwt/refs/heads/main/Script/Mics/MoreCamera.lua"))()
+  wait() 
+  loadstring(Game:HttpGet("https://raw.githubusercontent.com/EnverstNgau/NewFreeScript/refs/heads/main/Misc/SuperFpsBoost.lua"))()
+end 
