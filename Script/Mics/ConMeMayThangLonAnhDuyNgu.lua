3f03d5aff8c2ff067d677c1fb13f91719132449d
Enverst <123556098+EnverstNgau@users.noreply.github.com>
2025-01-07 18:20:52 +0700

    Delete Script/Mics/ConMeMayThangLonAnhDuyNgu.lua

--- Script/Mics/ConMeMayThangLonAnhDuyNgu.lua
+++
@@ -1,3 +0,0 @@
-getgenv().Key = "MARU-WUDQL-3HZJR-93VS-LEPSG-EUAA"
-getgenv().id = "959317224101908480"
-loadstring(game:HttpGet("https://raw.githubusercontent.com/xshiba/MaruBitkub/main/Mobile.lua"))()
